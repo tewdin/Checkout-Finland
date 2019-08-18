88bcb0a51ff76f270befe5b03bc787496398d03b
Timo Anttila <tewdin@users.noreply.github.com>
2019-08-18 14:13:20 +0300

    All the issues fixed

--- payment.php
+++ payment.php
@@ -3,159 +3,149 @@
 PHP integration for Checkout Finland
 @author: Timo Anttila, info@tuspe.com
 */
+header('Content-Type: text/html; charset=utf-8');
 
-$cart_data = array(
-    "checkout_total" => 90,
-    "products" => array(
-        "items" => array(
-            array(
+$cart_data = [
+    "checkout_total" => 90.00,
+    "products" => [
+        "items" => [
+            [
                 "title" => "Test 1",
-                "sku" => 1234,
-                "price" => 15,
+                "sku" => "1234",
+                "price" => 15.00,
                 "amount" => 2,
-                "total" => 30,
+                "total" => 37.20,
                 "vat" => 24
-            ),
-            array(
+            ],
+            [
                 "title" => "Test 2",
-                "sku" => 2468,
-                "price" => 20,
+                "sku" => "2468",
+                "price" => 20.00,
                 "amount" => 3,
-                "total" => 60,
+                "total" => 66.00,
                 "vat" => 10
-            )
-        ),
-        "test" => array(
+            ]
+        ],
+        "test" => [
             1234,
             2468
-        )
-    ),
-    "customer" => array(
+        ]
+    ],
+    "customer" => [
         "name" => "Timo Anttila",
         "phone" => "+35841234567",
         "email" => "info@example.com",
         "street" => "Testikuja 8",
-        "postal" => 21600,
+        "postal" => "21600",
         "area" => "Rauma",
         "delivery_street" => "Testikuja 8",
-        "delivery_postal" => 21600,
+        "delivery_postal" => "21600",
         "delivery_area" => "Rauma"
-    )
-);
+    ]
+];
+$lang = "fi";
 
-// Function for HMAC (made by CF)
-function calculateHmac($secret, $params, $body = '')
-{
-    // Keep only checkout- params, more relevant for response validation. Filter query
-    // string parameters the same way - the signature includes only checkout- values.
-    $includedKeys = array_filter(array_keys($params), function ($key) {
-        return preg_match('/^checkout-/', $key);
-    });
+// Use test IDs if no merchant IDs are found (ProcessWire)
+if($page->shop_merchant_id) $merchant = $page->shop_merchant_id; else $merchant = "375917";
+if($page->shop_merchant_secret) $password = $page->shop_merchant_secret; else $password = "SAIPPUAKAUPPIAS";
 
-    // Keys must be sorted alphabetically
-    sort($includedKeys, SORT_STRING);
-
-    $hmacPayload =
-        array_map(
-            function ($key) use ($params) {
-                return join(':', [ $key, $params[$key] ]);
-            },
-            $includedKeys
-        );
-
-    array_push($hmacPayload, $body);
-
-    return hash_hmac('sha256', join("\n", $hmacPayload), $secret);
-}
-
-// Use test IDs if no merchant IDs are found
-if($page->shop_merchant_id) $merchant = $page->shop_merchant_id; else $merchant .= "375917";
-if($page->shop_merchant_secret) $password = $page->shop_merchant_secret; else $password .= "SAIPPUAKAUPPIAS";
-
-// Explode the customer's name
+// Explode the customer's name and fix for the company ID if needed
 $name = explode(" ", $cart_data["customer"]["name"]);
 if($cart_data["customer"]["Company_id"]) $company_id = "FI". str_replace("-", "", $cart_data["customer"]["Company_id"]); else $company_id = "";
 
-// Current time for stamp and reference
+// Current time
 $time = time();
 
 // Foreach products to array
 $products = array();
-foreach($cart_data["products"][0] as $item){
+foreach($cart_data["products"]["items"] as $item){
     $products[] = array(
         "unitPrice" => $item["price"],
         "units" => $item["amount"],
         "vatPercentage" => $item["vat"],
         "productCode" => $item["sku"],
-        "deliveryDate" => $time,
         "description" => $item["title"],
-        "category" => $item["category"],
+//      "category" => $item["category"],
+        "deliveryDate" => date("Y-m-d", strtotime("+2 Weeks")),
         "merchant" => $merchant,
-        "stamp" => $time,
-        "reference" => $sku
+        "stamp" => "$time",
+        "reference" => $item["sku"]
     );
 }
 
 // Generate array for JSON
-$json = json_encode(array(
+$json = json_encode([
     "stamp" => $merchant . $time,
-    "reference" => $time,
-    "amount" => $cart_data["checkout_total"] * 100,
+    "reference" => "$time",
+    "amount" => $cart_data["checkout_total"],
     "currency" => "EUR",
-    "language" => $lang,
+    "language" => strtoupper($lang),
     "items" => $products,
-    "customer" => array(
+    "customer" => [
         "email" => $cart_data["customer"]["email"],
         "firstName" => $name[0],
         "lastName" => $name[1],
         "phone" => $cart_data["customer"]["phone"],
         "vatId" => $company_id
-    ),
-    "deliveryAddress" => array(
+    ],
+    "deliveryAddress" => [
         "streetAddress" => $cart_data["customer"]["delivery_street"],
         "postalCode" => $cart_data["customer"]["delivery_postal"],
         "city" => $cart_data["customer"]["delivery_area"],
 //      "county" => $cart_data["customer"]["delivery_street"],
         "country" => "FI"
-    ),
-    "invoicingAddress" => array(
+    ],
+    "invoicingAddress" => [
         "streetAddress" => $cart_data["customer"]["street"],
         "postalCode" => $cart_data["customer"]["postal"],
         "city" => $cart_data["customer"]["area"],
         "country" => "FI"
-    ),
-    "redirectUrls" => array(
+    ],
+    "redirectUrls" => [
         "success" => "https://example.com/checkout",
         "cancel" => "https://example.com/checkout"
-    ),
-    "callbackUrls" => array(
+    ],
+    "callbackUrls" => [
         "success" => "https://example.com/checkout",
         "cancel" => "https://example.com/checkout"
-    )
-));
+    ]
+], JSON_UNESCAPED_SLASHES);
 
 $headers = [
-    "checkout-account" => $merchant,
-    "checkout-algorithm" => "sha256",
-    "checkout-method" => "post",
-    "checkout-nonce" => $merchant . $time,
-    "checkout-timestamp" => date("c"),
-    "content-type" => "application/json; charset=utf-8"
+    "checkout-account:$merchant",
+    "checkout-algorithm:sha256",
+    "checkout-method:POST",
+    "checkout-nonce:". $merchant . $time,
+    "checkout-timestamp:".  date("Y-m-d\TH:i:s.000\Z", time())
 ];
 
-$headers['signature'] = calculateHmac($password, $headers, $json);
+// JSON body to $check
+$check = array_push($headers, $json);
 
-$ch = curl_init("https://api.checkout.fi/payments");
-curl_setopt_array($ch, array(
-    CURLOPT_POST => TRUE,
-    CURLOPT_RETURNTRANSFER => TRUE,
+// Content type and signature to the headers
+$headers[] = "content-type:application/json;charset=utf-8";
+$headers[] = "signature:". hash_hmac('sha256', join("\n", $check), $password);
+
+// Send
+$opt = array (
+    CURLOPT_URL => "https://api.checkout.fi/payments",
+    CURLOPT_PROXY => $proxy,
+    CURLOPT_POST => true,
+    CURLOPT_VERBOSE => true,
     CURLOPT_HTTPHEADER => $headers,
+    CURLOPT_RETURNTRANSFER => true,
     CURLOPT_POSTFIELDS => $json
-));
-$result=curl_exec ($ch);
-curl_close ($ch);
-$result = json_decode($result, true);
+);
 
+$ch = curl_init();
+curl_setopt_array($ch, $opt);
+$result = json_decode(curl_exec($ch),true);
+curl_close($ch);
+
+/*
+header("Location: ". $result["href"]);
+die();
+*/
 echo "<pre>";
 print_r($result);
 echo "</pre>";
